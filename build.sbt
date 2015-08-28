ef1fc1d8d891d2f7d649cd6fc910316c55f809ac
hitman <hitman@hitman.com>
2015-08-28 03:06:53 +0000

    the sbt things

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "hitman's fp journey"
+
+version := "0.0.1"
+
+scalaVersion := "2.11.6"
+
+
+
---
+++ src/main/scala/Test.scala
@@ -0,0 +1,9 @@
+class Test {
+	
+}
+
+object Test {
+	def main(args: Array[String]) = {
+		println("Yo, fp!")
+	}
+}
